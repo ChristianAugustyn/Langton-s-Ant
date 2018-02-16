145455509f63a8373dd1a6d5e0b6d22c759f24b5
ChristianAugustyn <35554085+ChristianAugustyn@users.noreply.github.com>
2018-02-15 23:07:47 -0500

    PyGame Implimentation

--- LangtonsAnt.py
+++ LangtonsAnt.py
@@ -1,4 +1,10 @@
-  
+#Author: Christian Augustyn
+#File Name: LangtonsAnt.py 
+#Date: Feb 7, 2018
+#Summary: Moves an "ant" base of a set of decisions to create a pattern
+
+import sys, pygame
+
 class Ant:
     
     def __init__(self, x, y, dir, grid): # creates an object with an(x, y) pos, the directio tne ant is facing and the state of the square the ant is on
@@ -107,17 +113,46 @@ def genArray(length):
             array[y].append(" ")
     return array
             
-   
+#initial variables needed for Ant class   
 length = 100
 grid = genArray(length)
 direction = 0
 ant = Ant(50, 50, direction, grid)
 
+#Initial Variables needed for pygame
+size = length, width = 1000, 1000
+white = 255, 255, 255
+black = 0, 0, 0
+clock = pygame.time.Clock()
+window = pygame.display.set_mode(size)
+tile = 10
+pygame.display.set_caption("Langton's Ant Simulation")
+
+#Game loop
 inBounds = True
 while inBounds is True:
     inBounds = ant.checkBounds()
     ant.run()
-    ant.printArray()
+    #if pygame is not installed replace all code below with ant.printArray()
+    #this will create a view in the console instead
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+        
+    window.fill(white)
+    for y in range(0, len(grid)):
+        for x in range(0, len(grid[y])):
+            rect = pygame.Rect((x * tile, y * tile),(tile ,tile))
+            if ant.grid[y][x] == " ":
+                pygame.draw.rect(window, white, rect)
+            
+            elif ant.grid[y][x] == "X":
+                pygame.draw.rect(window, black, rect)
+                
+    pygame.display.update()
+    clock.tick(60)
+
+
     
     
 
